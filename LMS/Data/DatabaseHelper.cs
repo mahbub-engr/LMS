5f70b1657b4b6ff60c8de26be4f7ed374bd5a771
Mahbub <mahbubalam12890@gmail.com>
2026-02-25 17:11:37 +0600

    create getbooks method and update choice and exit from console projet

--- LMS/Data/DatabaseHelper.cs
+++ LMS/Data/DatabaseHelper.cs
@@ -2,6 +2,7 @@
 using LMS.Service;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -20,15 +21,28 @@ namespace LMS.Data
             using (SqlConnection connection = new SqlConnection(ConnectionString)) {
                 string query = @"insert into tbl_Books(Title,Category,BasePrice,Charge,FinalPrice) values(@Title,@Category,@BasePrice,@Charge,@FinalPrice)";
                 using SqlCommand cmd = new SqlCommand(query, connection);
-                cmd.Parameters.AddWithValue("Title",books.title);
+                cmd.Parameters.AddWithValue("Title", books.title);
                 cmd.Parameters.AddWithValue("BasePrice", books.basicPrice);
                 cmd.Parameters.AddWithValue("Category", books.bookType);
                 cmd.Parameters.AddWithValue("Charge", books.maintainCost);
-                cmd.Parameters.AddWithValue("FinalPrice", Billing.bill(books.basicPrice,books.maintainCost));
+                cmd.Parameters.AddWithValue("FinalPrice", Billing.bill(books.basicPrice, books.maintainCost));
                 connection.Open();
                 cmd.ExecuteNonQuery();
                 connection.Close();
             }
         }
-    }
+
+        // show data method on console
+        public DataTable GetAllBooks()
+        {
+            DataTable dataTable = new DataTable();
+            using (SqlConnection connection = new SqlConnection(ConnectionString)) {
+                string query = @"Select * from tbl_Books";
+                using (SqlDataAdapter dataAdapter = new SqlDataAdapter(query, connection))
+                {
+                    dataAdapter.Fill(dataTable);
+                }
+            }
+            return dataTable;
+    }   }
 }
--- LMS/Program.cs
+++ LMS/Program.cs
@@ -2,6 +2,7 @@
 using LMS.Data;
 using LMS.Entities;
 using LMS.Service;
+using System.Data;
 using static System.Reflection.Metadata.BlobBuilder;
 DatabaseHelper dbHelper = new DatabaseHelper();
 Books book;
@@ -17,14 +18,23 @@ int choice;
 
 
 
-
 do
 {
     Console.WriteLine("1.Add Book:");
     Console.WriteLine("2.Show Book:");
+    Console.WriteLine("3.Exit:");
+    Console.Write("Enter Your Choice:");
     choice =Convert.ToInt32(Console.ReadLine());
+    if (choice == 3)
+    {
+        System.Environment.Exit(1);
+        break;
+    }
+
     switch (choice)
     {
+
+
         case 1:
 
             Console.Write("Enter Book Title:");
@@ -57,7 +67,22 @@ do
             dbHelper.AddBook(book);
         break;
         case 2:
+            DataTable booksTable = dbHelper.GetAllBooks();
+            foreach (DataRow row in booksTable.Rows)
+            {
+                Console.WriteLine($"Book ID: {row["BookID"]}");
+                Console.WriteLine($"Title: {row["Title"]}");
+                Console.WriteLine($"Type: {(BookType)Convert.ToInt32(row["Category"])}");
+                Console.WriteLine($"Base Price: {row["BasePrice"]}");
+                Console.WriteLine($"Maintain Cost: {row["Charge"]}");
+                Console.WriteLine($"Final Price: {row["FinalPrice"]}");
+                Console.WriteLine("----------------------");
 
-        break;
+
+            }
+            break;
+        default:
+            Console.WriteLine("Invalid Choice");
+        continue;
     }
-}while (true);
+} while (true);
